877610311069326cd62ffaf1048d14a6fa19f599
Andreviga <andre_vigarani@hotmail.com>
2026-06-03 21:44:49 -0300

    Configurar Supabase do bolao

--- backend-config.js
+++ backend-config.js
@@ -1,7 +1,7 @@
 window.BOLAO_BACKEND = {
   provider: "supabase",
-  url: "",
-  anonKey: "",
+  url: "https://cvoyaykuhlfstpqgwzfd.supabase.co",
+  anonKey: "sb_publishable_x2IRZsjc6rBglzY9lOQ4vQ_xAyTN7DT",
   table: "bolao_state",
   rowId: "copa2026-familia"
 };
